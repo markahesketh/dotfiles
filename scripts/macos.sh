456b928cc6ff91fcd7cc22f008c36a7bfd0b97b8
Mark Hesketh <contact@markhesketh.co.uk>
2024-06-07 23:12:57 +0100

    Install mise via brew

--- scripts/macos.sh
+++ scripts/macos.sh
@@ -16,7 +16,7 @@ then
   if test "$(uname)" = "Darwin"
   then
     echo "Installing Homebrew ..."
-    ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
   fi
 fi
 echo ""
