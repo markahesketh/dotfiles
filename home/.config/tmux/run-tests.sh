4b46bd95e4384ed94a79ca632b3247da29d8930a
Mark Hesketh <contact@markhesketh.co.uk>
2026-02-24 22:48:19 +0000

    feat: enhance tmux test runner with output scrolling and all-tests binding

--- home/.config/tmux/run-tests.sh
+++ home/.config/tmux/run-tests.sh
@@ -1,16 +1,27 @@
 #!/usr/bin/env bash
 
-if [[ -f .rspec || -d spec ]]; then
-  bundle exec rspec --fail-fast
-elif [[ -d test ]]; then
-  if [[ -f bin/rails ]]; then
-    ./bin/rails test --fail-fast
-  else
-    bundle exec ruby -Itest -e 'Dir["test/**/*_test.rb"].each { |f| require_relative f }'
-  fi
-else
-  echo "No test suite detected (no spec/ or test/ directory found)"
-fi
+ALL=${1:-false}
 
-echo ""
-read -rp "Press enter to close..."
+tmpfile=$(mktemp)
+trap 'rm -f "$tmpfile"' EXIT
+
+run_tests() {
+  if [[ -f .rspec || -d spec ]]; then
+    bundle exec rspec --fail-fast
+  elif [[ -d test ]]; then
+    if [[ -f bin/rails ]]; then
+      if [[ "$ALL" == "true" ]]; then
+        ./bin/rails test:all --fail-fast
+      else
+        ./bin/rails test --fail-fast
+      fi
+    else
+      bundle exec ruby -Itest -e 'Dir["test/**/*_test.rb"].each { |f| require_relative f }'
+    fi
+  else
+    echo "No test suite detected (no spec/ or test/ directory found)"
+  fi
+}
+
+run_tests 2>&1 | tee "$tmpfile"
+less --mouse -R +G "$tmpfile"
